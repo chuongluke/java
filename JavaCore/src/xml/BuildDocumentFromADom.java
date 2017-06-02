1ee9311c5325904e826d61f9c33af439b6d3ea22
DTT0001 <chuongluke@gmail.com>
2017-06-02 14:31:25 +0700

    Build document from a dom

---
+++ JavaCore/src/xml/BuildDocumentFromADom.java
@@ -0,0 +1,32 @@
+package xml;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.jdom2.input.DOMBuilder;
+
+/**
+ * Build document from a dom
+ * 
+ * @author no_name
+ *
+ */
+public class BuildDocumentFromADom {
+
+	public static void main(String[] args) {
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			factory.setNamespaceAware(true);
+			
+			DocumentBuilder domBuilder = factory.newDocumentBuilder();
+			Document w3cDocument = domBuilder.parse("test.xml");
+			
+			DOMBuilder jdomBuilder = new DOMBuilder();
+			org.jdom2.Document jdomDocument = jdomBuilder.build(w3cDocument);
+			System.out.println(jdomDocument);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
